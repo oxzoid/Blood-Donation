9daae0b0e69d551aeae9bf32e75d8a7aaaf2225c
oxzoid <pranavdevkhindria@gmail.com>
2024-01-20 19:11:19 +0530

    Signup ui in react

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Signup from './Signup'
+
+function App() {
+
+  return (
+    <>
+     <div>
+      <Signup />
+      </div>  
+    </>
+  )
+}
+
+export default App
